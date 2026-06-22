91e8ca46e4560a9b7f78f7b120a960f38c238981
Kirifox <uwfshawn@gmail.com>
2026-06-22 14:04:10 -0400

    added player and movement for player

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 import constants
 from logger import log_state
+from player import Player
 def main():
     print("Hello from asteroids!")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
+    #initiating game, declaring variables and groups necessary to display game, player, and asteroids.
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    dt = 0.0
+    ship = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    
+    #gameplay loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for item in updatable:
+            item.update(dt)
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 
+
 
 if __name__ == "__main__":
     main()
